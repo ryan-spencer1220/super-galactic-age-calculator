dc85f23cb1d139f4438e17e313bcd2b36bed554d
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-02-04 10:24:09 -0800

    Create UI logic to display ages on different planets

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import $ from "jquery";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import UserAge from "./js/calculator.js";
+
+$(document).ready(function () {
+  $("form#new-form").submit(function (event) {
+    event.preventDefault();
+    const inputAge = $("#age-entered").val();
+    const inputExpectancy = $("#expectancy-entered").val();
+    const userAge = new UserAge(inputAge, inputExpectancy);
+    const mercuryAge = userAge.mercuryCalculator();
+    const venusAge = userAge.venusCalculator();
+    const marsAge = userAge.marsCalculator();
+    const jupiterAge = userAge.jupiterCalculator();
+    const lifeExpectancyArrayByPlanet = lifeExpectancyCalculator();
+    $("#mercury-age").text(mercuryAge);
+    $("#venus-age").text(venusAge);
+    $("#mars-age").text(marsAge);
+    $("#jupiter-age").text(jupiterAge);
+    $("#life-expectancy-array").text(lifeExpectancyArrayByPlanet);
+  });
+});
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -20,18 +20,18 @@ export default class UserAge {
   }
 
   lifeExpectancyCalculator() {
-    let mercuryExpectancy = Math.floor(
-      this.lifeExpectancy / 0.24 - this.age / 0.24
-    ); // 195
-    let venusExpectancy = Math.floor(
-      this.lifeExpectancy / 0.62 - this.age / 0.62
-    ); // 76
-    let marsExpectancy = Math.floor(
-      this.lifeExpectancy / 1.88 - this.age / 1.88
-    ); // 25
-    let jupiterExpectancy = Math.floor(
-      this.lifeExpectancy / 11.86 - this.age / 11.86
-    ); // 4
+    let mercuryExpectancy = Math.abs(
+      Math.floor(this.lifeExpectancy / 0.24 - this.age / 0.24)
+    );
+    let venusExpectancy = Math.abs(
+      Math.floor(this.lifeExpectancy / 0.62 - this.age / 0.62)
+    );
+    let marsExpectancy = Math.abs(
+      Math.floor(this.lifeExpectancy / 1.88 - this.age / 1.88)
+    );
+    let jupiterExpectancy = Math.abs(
+      Math.floor(this.lifeExpectancy / 11.86 - this.age / 11.86)
+    );
     let lifeExpectancyArray = [
       mercuryExpectancy,
       venusExpectancy,
